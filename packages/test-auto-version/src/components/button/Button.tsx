e006d60cd9caa762164f14c8bcf17d594792d87f
Danial Dinh <danialdinh@gmail.com>
2025-02-25 19:17:10 +0700

    feat: 0.0.0.6-alpha.0

--- packages/test-auto-version/src/components/button/Button.tsx
+++ packages/test-auto-version/src/components/button/Button.tsx
@@ -1,7 +1,7 @@
 function Button() {
   return (
     <div className="a8-pkg-text-primary a8-pkg-px-5 a8-pkg-py-2 a8-pkg-rounded-full">
-      Connect Here
+      Connect Here ver 0.0.6-alpha.0
     </div>
   )
 }
